b498890736c09dfaae6b614f157efe7a34389e68
ravik <ravik@host.docker.internal>
2023-06-28 22:33:03 -0400

    Created Suite and Collection

---
+++ Test Suites/TestCloud_MobileKatalon_Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestCloud_MobileKatalon_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>daeaf45c-8c2c-460f-abea-a36b7f71eb83</testSuiteGuid>
+   <testCaseLink>
+      <guid>aa90c366-b4a5-40e1-a780-9598f35fcb97</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Demo 2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
